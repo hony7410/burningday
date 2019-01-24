c53399a58dc3b0977c5a0fdde715ba89addf7ec2
jihoon <hony7410@naver.com>
2019-01-24 17:32:15 +0900

    화면변경

--- src/main/java/kr/co/koscom/marketdata/controller/TrendDataController.java
+++ src/main/java/kr/co/koscom/marketdata/controller/TrendDataController.java
@@ -2,17 +2,18 @@ package kr.co.koscom.marketdata.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import kr.co.koscom.marketdata.api.LogpressoApiCaller;
 import kr.co.koscom.marketdata.api.MarketDataApiCaller;
 import kr.co.koscom.marketdata.api.TrendDataApiCaller;
+import kr.co.koscom.marketdata.model.SearchData;
 
 @Controller
 public class TrendDataController {
@@ -28,9 +29,25 @@ public class TrendDataController {
 	
     @RequestMapping(path = "/trenddata/company/{issueKeyword}",
     		method = { RequestMethod.GET, RequestMethod.POST } )
-    public @ResponseBody void priceJson(@PathVariable String issueKeyword) {
-        trendDataApiCaller.getSearchData(issueKeyword);
-        return;
+    public @ResponseBody String trendSearchJson(@PathVariable String issueKeyword) {
+    	SearchData[] searchDataArr = trendDataApiCaller.getSearchData(issueKeyword);
+    	for(SearchData s : searchDataArr){
+    		System.out.println(s.toString());
+    	}
+    	
+    	ObjectMapper mapper = new ObjectMapper();
+    	//Object to JSON in String
+    	try {
+			String jsonInString = mapper.writeValueAsString(searchDataArr);
+			System.out.println("I will return");
+			System.out.println(jsonInString);
+			return jsonInString;
+		} catch (JsonProcessingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    	
+        return null;
     }
     
  }
